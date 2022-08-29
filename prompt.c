814e1a74e4b3ffffab4dd639062d59f97064ab5a
IwegbuJeddy <iJeddy5.0@gmail.com>
2022-08-29 04:45:00 -0700

    prompt component added

--- prompt.c
+++ prompt.c
@@ -2,10 +2,10 @@
 int main(void)
 {
 char *buffer = NULL;
-ssize_t read_num;
+//ssize_t read_num;
 size_t n = 0;
 printf("@ ");
-read_num = getline(&buffer, &n, stdin);
+getline(&buffer, &n, stdin);
 printf("###%s\n", buffer);
-printf("%ld\n", read_num);
+//printf("%ld\n", read_num);
 }
